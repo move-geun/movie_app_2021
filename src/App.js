6b15293e7b13f383768c13465067f3c94351c161
move-geun <movegeun11@gmail.com>
2021-09-01 14:52:52 +0900

    컴포넌트 라이프 사이클에 따른, didMount 설정과 셋타임아웃 설정

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import React from "react";
 
 class App extends React.Component {
   state = {
-    count: 0,
+    isLoading: true,
   };
-  add = () => {
-    console.log("Add");
-  };
-  minus = () => {
-    console.log("minus");
-  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 3000);
+  }
+
   render() {
+    const { isLoading } = this.state;
     return (
       <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+        <h1> {isLoading ? "Loading..." : "WE ARE READY"}</h1>
       </div>
     );
   }
