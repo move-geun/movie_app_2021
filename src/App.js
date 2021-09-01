0622767d1a84321dbddcead835f76cb5f36d05cf
move-geun <movegeun11@gmail.com>
2021-09-01 13:04:15 +0900

    alt 와 key 설정

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ function Food({ name, image }) {
   return (
     <div>
       <h2> I like {name}</h2>
-      <img src={image} />
+      <img src={image} alt={name} />
     </div>
   );
 }
 
 const likeFood = [
   {
+    id: 0,
     name: "Kimchi",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
   },
   {
+    id: 1,
     name: "돈가스",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
@@ -24,7 +26,7 @@ function App() {
   return (
     <div>
       {likeFood.map((dish) => (
-        <Food name={dish.name} image={dish.image} />
+        <Food key={dish.id} name={dish.name} image={dish.image} />
       ))}
     </div>
   );
