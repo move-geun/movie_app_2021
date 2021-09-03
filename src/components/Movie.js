8e5605b25509dd7e25bc118fd3e7e864f1953753
move-geun <movegeun11@gmail.com>
2021-09-03 16:21:20 +0900

    Link 설정, react-router-dom 설치 및 활용

---
+++ src/components/Movie.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import "./movie.css";
+
+function Movie({ id, year, title, summary, poster, genres }) {
+  return (
+    <Link
+      to={{
+        pathname: `/movie/${id}`,
+        state: { year, title, summary, poster, genres },
+      }}
+    >
+      <div className="movie">
+        <img className="movie__poster" src={poster} alt={title} />
+        <div className="movie__data">
+          <div className="movie__title">{title}</div>
+          <div className="movie__year">{year}</div>
+          <ul className="movie__genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <div className="movie__summary">{summary.slice(0, 180)}...</div>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
---
+++ src/routes/About.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function about(props) {
+  console.log(props);
+  return <span>About Pages</span>;
+}
+
+export default about;
